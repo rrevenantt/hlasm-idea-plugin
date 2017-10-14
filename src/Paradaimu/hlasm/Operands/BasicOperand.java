be20dbb66d9248f64144531e37f39be1a934abb7
CopperAv <CopperAvatar@gmail.com>
2017-10-14 17:29:13 +0300

    Update BasicOperand.java

--- src/Paradaimu/hlasm/Operands/BasicOperand.java
+++ src/Paradaimu/hlasm/Operands/BasicOperand.java
@@ -1,16 +1,32 @@
 package Paradaimu.hlasm.Operands;
 
 import org.jetbrains.annotations.NotNull;
-
+/**
+ * @author  Paradaimu
+ * 14.10.2017
+ * 
+ * Interface for any operand
+ */
 public interface BasicOperand {
+    /**
+     * return data
+    **/
     @NotNull
     byte[] getData();
-    
+    /**
+     * set new data
+    **/
     void setData(byte[] data);
-    
+    /**
+     * get data size (in bytes)
+    **/
     int getSize();
-
-    short getBits();
-    
+    /**
+     * get bits (0 - 8) for first byte of data
+    **/
+    byte getBits();
+    /**
+     * method for comparing first byte of new value and old data
+    **/
     boolean isEqualsToBits(byte firstByte);
 }
